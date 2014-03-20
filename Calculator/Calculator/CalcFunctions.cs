24d6f98b13c2e5126a9b03a60713c424df68b88a
Jay Tayade <koolguru2@gmail.com>
2014-03-20 02:03:33 -0700

    Main solution

---
+++ Calculator/Calculator/CalcFunctions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class CalcFunctions
+    {
+        public void Calcfunctions()
+        {
+        }
+        public void Add(int number1, int number2)
+        {
+            int addedNumbers = number1 + number2;
+        }
+    }
+}
